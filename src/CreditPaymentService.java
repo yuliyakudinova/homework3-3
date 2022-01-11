1f3ddb11bae6d71a41c14a633ec66b8640a98fdc
yulechka.kudinova@mail.ru <yulechka.kudinova.1991@mail.ru>
2022-01-11 15:53:14 +0300

    second commit

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,8 +1,4 @@
 public class CreditPaymentService {
-    int credit;
-    float loanRate;
-    int creditTerm;
-
     public float calculate(int credit, float loanRate, int creditTerm) {
         float monthlyRate = loanRate / 12 / 100;
         float payment = (float) (credit * ((monthlyRate * Math.pow((1 + monthlyRate), creditTerm)) / (Math.pow((1 + monthlyRate), creditTerm) - 1)));
